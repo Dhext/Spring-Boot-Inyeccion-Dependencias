813122e1bee063505a42c7bafd6185f1d0130553
Guillermo <luisg1.luis@gmail.com>
2019-05-20 14:14:50 -0500

    inicio

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/model/service/miServicio.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.di.app.model.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component("miServicioSimple")
+public class miServicio implements IServicio {
+	@Override
+	public String operacion() {
+		return "Ejecutando algun proceso importante...";
+	}
+}
